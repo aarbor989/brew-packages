fd132ca545b4319e125e38520f05cf57ca70c812
Austin Arbor <aarbor989@gmail.com>
2017-02-08 23:15:21 -0500

    updating brew install list

---
+++ brew_install.sh
@@ -0,0 +1,15 @@
+brew install bash-completion
+brew install dnsmasq
+brew install gdbm
+brew install git
+brew install jq
+brew install maven
+brew install oniguruma
+brew install openssl
+brew install pip-completion
+brew install python
+brew install python3
+brew install readline
+brew install sqlite
+brew install wget
+brew install xz
