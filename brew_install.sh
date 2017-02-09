d870a6f3978c0530896aba2d12c5ac2e2663965d
Austin Arbor <aarbor989@gmail.com>
2017-02-08 23:29:08 -0500

    updating brew install list

--- brew_install.sh
+++ brew_install.sh
@@ -13,4 +13,25 @@ brew install readline
 brew install sqlite
 brew install wget
 brew install xz
-brew cask install utorrent
+brew cask install cyberduck
+brew cask install dropbox
+brew cask install eclipse-jee
+brew cask install flycut
+brew cask install github-desktop
+brew cask install google-drive
+brew cask install intellij-idea
+brew cask install java
+brew cask install path-finder
+brew cask install pg-commander
+brew cask install plex-media-server
+brew cask install sequel-pro
+brew cask install slack
+brew cask install sourcetree
+brew cask install sublime-text
+brew cask install teamviewer
+brew cask install the-unarchiver
+brew cask install tunnelblick
+brew cask install unrarx
+brew cask install virtualbox
+brew cask install vlc
+brew cask install xquartz
