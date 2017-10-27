48de3aee5f7cccd274cad2374f5338091addd976
Austin Arbor <aarbor989@gmail.com>
2017-10-27 00:18:35 -0400

    updating brew install list

--- brew_install.sh
+++ brew_install.sh
@@ -1,16 +1,23 @@
 brew install bash-completion
+brew install bash-snippets
 brew install brew-cask-completion
 brew install dnsmasq
 brew install gdbm
 brew install git
+brew install gradle
 brew install jq
 brew install makedepend
 brew install maven
 brew install maven-completion
+brew install nmap
 brew install oniguruma
 brew install openssl
+brew install openssl@1.1
 brew install pcre
 brew install pip-completion
+brew install pkg-config
+brew install protobuf
+brew install protobuf-c
 brew install python
 brew install python3
 brew install readline
@@ -19,24 +26,32 @@ brew install wget
 brew install xz
 brew install z
 brew install zsh
+brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
+brew cask install airflow
+brew cask install atom
 brew cask install beyond-compare
 brew cask install cyberduck
+brew cask install docker
 brew cask install dropbox
 brew cask install eclipse-jee
 brew cask install flycut
 brew cask install github-desktop
 brew cask install google-drive
+brew cask install handbrake
 brew cask install intellij-idea
 brew cask install iterm2
 brew cask install java
+brew cask install keka
 brew cask install mysqlworkbench
 brew cask install path-finder
 brew cask install pg-commander
 brew cask install plex-media-server
+brew cask install rekordbox
 brew cask install sequel-pro
 brew cask install slack
 brew cask install sourcetree
+brew cask install sqlworkbenchj
 brew cask install sublime-text
 brew cask install teamviewer
 brew cask install the-unarchiver
